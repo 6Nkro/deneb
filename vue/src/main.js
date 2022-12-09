f0f47974f18c14cd35c07e4c6f9511ea5a5e6c71
hub-coco <kij1467@gmail.com>
2022-12-09 10:18:09 +0900

    Modify properties

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,17 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import BootstrapVue3 from 'bootstrap-vue-3'
 import common from './assets/common.js'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
-createApp(App)
-  .use(store)
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
+
+app.use(store)
   .use(router)
-  .use(common)
+  .use(BootstrapVue3)
   .use(VueSweetalert2)
+  .use(common)
   .mount('#app')
